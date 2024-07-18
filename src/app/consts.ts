8f6ee2351e18c93e5eed9325b2dbbd1497b7a9bf
ktiays <cuansky@gmail.com>
2024-07-18 11:13:34 +0800

    Remove premium and event tracking & update OpenAI api models & upgrade yarn

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -94,10 +94,10 @@ export const CHATBOTS: Record<BotId, { name: string; avatar: string }> = {
 }
 
 export const CHATGPT_HOME_URL = 'https://chat.openai.com'
-export const CHATGPT_API_MODELS = ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo'] as const
+export const CHATGPT_API_MODELS = ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo', 'gpt-4o'] as const
 export const ALL_IN_ONE_PAGE_ID = 'all'
 
 export const DEFAULT_CHATGPT_SYSTEM_MESSAGE =
-  'You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: 2021-09-01. Current date: {current_date}'
+  'You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: 2023-12-01. Current date: {current_date}'
 
 export type Layout = 2 | 3 | 4 | 'imageInput' | 'twoVertical' | 'sixGrid' // twoVertical is deprecated
